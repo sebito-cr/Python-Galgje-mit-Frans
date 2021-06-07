701173eb90c072cee169e48a686990713e75866f
Frans Vaes <>
2021-06-07 09:50:17 +0000

    Implementeer begin geraden letters en levens

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 print("Welkom bij galgje")
 import random
 WoordLijst = ['informatica','informatiekunde','spelletje','aardigheidje','scholier','fotografie','waardebepaling','specialiteit','verzekering','universiteit','heesterperk']
+GeheimWoord = WoordLijst[random.randrange(0, len(WoordLijst))]
+GeradenLetters = []
+levens = 5
