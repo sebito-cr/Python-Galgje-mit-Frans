7e465638c480f45c41f6eff118d66a3972e83d96
Sebito Liu <>
2021-06-08 21:15:07 +0000

    Implementeer random woord beslisser

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ WoordLijst = ['informatica','informatiekunde','spelletje','aardigheidje','scholi
 GeheimWoord = WoordLijst[random.randrange(0, len(WoordLijst))]
 GeradenLetters = []
 levens = 5
+
+def checkWin():
+    for i in range(len(GeheimWoord)):
+        if not GeheimWoord[i] in GeradenLetters:
+            return(False)
+    return(True)
+
